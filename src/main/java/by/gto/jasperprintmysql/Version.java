00231d553bb88631fab5aaf5bd0c964e38bcfaa1
Gogs <gogs@fake.local>
2017-03-17 16:09:32 +0300

    del '.idea'

--- src/main/java/by/gto/jasperprintmysql/Version.java
+++ src/main/java/by/gto/jasperprintmysql/Version.java
@@ -2,10 +2,10 @@ package by.gto.jasperprintmysql;
 
 public final class Version {
 
-    private static final String VERSION = "0.2.29";
+    private static final String VERSION = "0.2.30";
     private static final String GROUPID = "by.gto";
     private static final String ARTIFACTID = "btoReportNG";
-    private static final String DATEBUILD = "2016-10-05 09:33";
+    private static final String DATEBUILD = "2017-03-17 11:33";
 
     public static String getDATEBUILD() {
         return DATEBUILD;
