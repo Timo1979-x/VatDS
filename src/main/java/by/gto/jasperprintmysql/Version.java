137de13cf50841e4300668e04a8b4f6e0e1a97e1
Tim Lukashevich <efreet@tut.by>
2017-07-18 08:11:54 +0300

    Добавлены файлы Заметки.txt и реестр договоров.xlsm

--- src/main/java/by/gto/jasperprintmysql/Version.java
+++ src/main/java/by/gto/jasperprintmysql/Version.java
@@ -2,10 +2,10 @@ package by.gto.jasperprintmysql;
 
 public final class Version {
 
-    private static final String VERSION = "0.3.6";
+    private static final String VERSION = "0.3.7";
     private static final String GROUPID = "by.gto";
     private static final String ARTIFACTID = "btoReportNG";
-    private static final String DATEBUILD = "2017-04-17 09:48";
+    private static final String DATEBUILD = "2017-05-12 09:07";
 
     public static String getDATEBUILD() {
         return DATEBUILD;
